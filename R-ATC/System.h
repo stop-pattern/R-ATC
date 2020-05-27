8074ca4ac6380a88a42f77fd580366d14db2d53d
stop-pattern <railwaysandstationfan+Git@gmail.com>
2020-05-28 00:39:27 +0900

    [add] セッターの宣言
    やる気が出なかったから実装は今度
    中身はやるだけ

--- R-ATC/System.h
+++ R-ATC/System.h
@@ -14,6 +14,7 @@ private:
 	std::filesystem::path dllPath;	// このdllへの絶対パス
 	std::filesystem::path iniPath;	// このdllと同階層同名のiniへの絶対パス
 	std::map<std::string, std::map<std::string, int>> iniData;	// iniから読み出した値
+	VehicleState vehicleStatus;	// 車両状態値
 
 	/* ----- functions ----- */
 	// get dll path (with ini path)
@@ -61,5 +62,54 @@ public:
 	// return : int
 	//     whether app && key are found (if not found, -1)
 	int getiniData(std::string app, std::string key);
-};
 
+	// save vehicle spec to vehicleStatus
+	// called in SetVehicleSpec(Spec)
+	// arg : Spec sp
+	//     vehicle spec
+	void SetVehicleSpec(Spec sp);
+
+	// save manual handle position to vehicleStatus
+	// called in SetPower(int)
+	// arg : int p
+	//     power notch position
+	void SetPower(int p);
+
+	// save manual handle position to vehicleStatus
+	// called in SetBrake(int)
+	// arg : int p
+	//     brake notch position
+	void SetBrake(int p);
+
+	// save manual reverser position to vehicleStatus
+	// called in SetReverser(int)
+	// arg : int p
+	//     reverser position
+	void SetReverser(int p);
+
+	// save key status to vehicleStatus
+	// called in KeyDown(int)
+	// arg : int k
+	//     key status
+	void KeyDown(int k);
+
+	// save key status to vehicleStatus
+	// called in KeyUp(int)
+	// arg : int k
+	//     key status
+	void KeyUp(int k);
+
+	// save door status to vehicleStatus
+	// called in DoorOpen(void)
+	void DoorOpen(void);
+
+	// save door status to vehicleStatus
+	// called in DoorClose(void)
+	void DoorClose(void);
+
+	// save signal index to vehicleStatus
+	// called in SetSignal(int)
+	// arg : int s
+	//     signal index
+	void SetSignal(int s);
+};
