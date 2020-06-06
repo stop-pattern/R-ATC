66daf6dfd97ee25bcbb2dd94d00cb0ef567e511c
stop-pattern <railwaysandstationfan+Git@gmail.com>
2020-06-07 00:32:00 +0900

    [add] 車両状態更新

--- R-ATC/dllmain.cpp
+++ R-ATC/dllmain.cpp
@@ -1,5 +1,8 @@
 ﻿// dllmain.cpp : DLL アプリケーションのエントリ ポイントを定義します。
 #include "pch.h"
+#include "Plugin.h"
+
+Plugin* atsPlugin = new Plugin();
 
 BOOL APIENTRY DllMain(HMODULE hModule,
 	DWORD  ul_reason_for_call,
@@ -10,8 +13,11 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 	{
 	case DLL_PROCESS_ATTACH:
 	case DLL_THREAD_ATTACH:
+		atsPlugin->Attach(hModule);
+		break;
 	case DLL_THREAD_DETACH:
 	case DLL_PROCESS_DETACH:
+		delete atsPlugin;
 		break;
 	}
 	return TRUE;
@@ -30,54 +36,54 @@ DE int SC GetPluginVersion(void) {
 	return PI_VERSION;
 }
 
-DE void SC SetVehicleSpec(Spec) {
-
+DE void SC SetVehicleSpec(Spec sp) {
+	atsPlugin->SetVehicleSpec(sp);
 }
 
 DE void SC Initialize(int) {
 
 }
 
-DE Hand SC Elapse(State, int*, int*) {
+DE Hand SC Elapse(State st, int* p, int* s) {
 	return {};
 }
 
-DE void SC SetPower(int) {
-
+DE void SC SetPower(int p) {
+	atsPlugin->SetPower(p);
 }
 
-DE void SC SetBrake(int) {
-
+DE void SC SetBrake(int b) {
+	atsPlugin->SetBrake(b);
 }
 
-DE void SC SetReverser(int) {
-
+DE void SC SetReverser(int r) {
+	atsPlugin->SetReverser(r);
 }
 
-DE void SC KeyDown(int) {
-
+DE void SC KeyDown(int k) {
+	atsPlugin->KeyDown(k);
 }
 
-DE void SC KeyUp(int) {
-
+DE void SC KeyUp(int k) {
+	atsPlugin->KeyUp(k);
 }
 
-DE void SC HornBlow(int) {
+DE void SC HornBlow(int h) {
 
 }
 
 DE void SC DoorOpen(void) {
-
+	atsPlugin->DoorOpen();
 }
 
 DE void SC DoorClose(void) {
-
+	atsPlugin->DoorClose();
 }
 
-DE void SC SetSignal(int) {
-
+DE void SC SetSignal(int s) {
+	atsPlugin->SetSignal(s);
 }
 
-DE void SC SetBeaconData(Beacon) {
+DE void SC SetBeaconData(Beacon b) {
 
 }
