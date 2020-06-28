5bab52bd3f579ad20976379985bfaa85c6d9f5ca
stop-pattern <railwaysandstationfan+Git@gmail.com>
2020-06-29 00:45:05 +0900

    [add] TIMSもどき微修正

--- R-ATC/dllmain.cpp
+++ R-ATC/dllmain.cpp
@@ -65,7 +65,7 @@ DE Hand SC Elapse(State st, int* p, int* s) {
 	// 下からn桁目の数値をuint8_tで返す (nが1以外で0の時は10)
 	auto getDigitF = [&](float arg, uint16_t n)noexcept -> uint8_t {
 		if (n == 0) return arg;
-		float ret = arg;
+		float ret = std::fabs(arg);;
 		for (size_t i = 0; i < n - 1; i++) {
 			ret /= 10;
 		}
@@ -76,6 +76,11 @@ DE Hand SC Elapse(State st, int* p, int* s) {
 	};
 
 	float A = atsPlugin->getStatus().status.A;
+	int8_t u = 0;
+	if (ret.B == 0) {
+		if (ret.P == 0) u = 0;
+		else u = 1;
+	} else u = 2;
 
 	// TIMS代替
 	p[21] = st.V < 0 ? 2 : st.V > 0 ? 1 : 0;
@@ -83,31 +88,37 @@ DE Hand SC Elapse(State st, int* p, int* s) {
 	p[23] = getDigitF(st.V, 3);
 	p[24] = getDigitF(st.V, 2);
 	p[25] = getDigitF(st.V, 1);
-	p[26] = getDigitF(st.V, 0);
+	p[26] = std::fabs(st.V);
 	p[30] = ret.B;
 	p[37] = st.T / 3600000;
 	p[38] = st.T / 60000;
 	p[39] = st.T / 1000;
-	p[41] = A < 0 ? 2 : A > 0 ? 1 : 0;
-	p[42] = A < 0 ? 2 : A > 0 ? 1 : 0;
-	p[43] = A < 0 ? 2 : A > 0 ? 1 : 0;
-	p[44] = A < 0 ? 2 : A > 0 ? 1 : 0;
-	p[45] = getDigitF(st.V, 3);	// 3
-	p[46] = getDigitF(st.V, 2);	// 2
-	p[47] = getDigitF(st.V, 1);	// 1
-	p[50] = getDigitF(st.V, 0);	// needle
+	if (std::rand() % 2) p[41] = u;
+	if (std::rand() % 2) p[42] = u;
+	if (std::rand() % 2) p[43] = u;
+	if (std::rand() % 2) p[44] = u;
+	p[45] = getDigitF(st.V, 3);
+	p[46] = getDigitF(st.V, 2);
+	p[47] = getDigitF(st.V, 1);
+	p[50] = std::fabs(st.V);
 	p[100] = getDigitF(st.V, 3);
 	p[101] = getDigitF(st.V, 2);
 	p[102] = getDigitF(st.V, 1);
 	p[103] = 0;
 	p[104] = 0;
-	p[105] = A < 0 ? 2 : A > 0 ? 1 : 0;
-	p[106] = A < 0 ? 2 : A > 0 ? 1 : 0;
-	p[107] = A < 0 ? 2 : A > 0 ? 1 : 0;
-	p[108] = A < 0 ? 2 : A > 0 ? 1 : 0;
-	p[109] = A < 0 ? 2 : A > 0 ? 1 : 0;
+	if (std::rand() % 2) p[105] = u;
+	if (std::rand() % 2) p[106] = u;
+	if (std::rand() % 2) p[107] = u;
+	if (std::rand() % 2) p[108] = u;
+	p[109] = atsPlugin->getStatus().status.dZ < 0 ? 2 : atsPlugin->getStatus().status.dZ > 0 ? 1 : 0;
 	p[122] = atsPlugin->getDoor() ? 0 : st.BC < 200 ? 1 : 0;
-	p[122] = atsPlugin->getDoor() ? 0 : st.BC < 200 ? 1 : 0;
+	p[123] = st.BC / 20 > 00 ? st.BC / 20 < 10 ? st.BC / 20 - 00 : 10 : 10;
+	p[124] = st.BC / 20 > 10 ? st.BC / 20 < 20 ? st.BC / 20 - 10 : 10 : 10;
+	p[125] = st.BC / 20 > 20 ? st.BC / 20 < 30 ? st.BC / 20 - 20 : 10 : 10;
+	p[126] = st.BC / 20 > 30 ? st.BC / 20 < 40 ? st.BC / 20 - 30 : 10 : 10;
+	p[127] = (st.MR-750) / 5 > 00 ? (st.MR - 750) / 5 < 10 ? (st.MR - 750) / 20 - 00 : 10 : 10;
+	p[128] = (st.MR - 750) / 5 > 10 ? (st.MR - 750) / 5 < 20 ? (st.MR - 750) / 20 - 10 : 10 : 10;
+	p[129] = (st.MR - 750) / 5 > 20 ? (st.MR - 750) / 5 < 30 ? (st.MR - 750) / 20 - 20 : 10 : 10;
 
 	return atsPlugin->Elapse(ret);
 }
