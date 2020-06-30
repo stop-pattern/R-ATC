7f4b368b4c2eac99cda3a2d3284ee3b31be68dba
stop-pattern <railwaysandstationfan+Git@gmail.com>
2020-06-30 23:24:31 +0900

    [add] ブレーキ指令出力
    ついでにP演算関数作成

--- R-ATC/R_ATC.cpp
+++ R-ATC/R_ATC.cpp
@@ -1,6 +1,66 @@
 #include "pch.h"
 #include "R_ATC.h"
 
+uint16_t R_ATC::calclateSpeed(VehicleState state) {
+	uint16_t ret = UINT16_MAX;
+	for (size_t i = 0; i < static_cast<size_t>(limitIndex::size); i++) {
+		if (ret <= this->limit[i]) ret = this->limit[i];
+	}
+	return ret;
+}
+
+uint16_t R_ATC::calclateBrake(VehicleState state, uint16_t speed, uint8_t param) {
+	uint16_t ret = 0;	// ƒuƒŒ[ƒLw—ß’l
+	float psec = 2.5;	// PÚ‹ß•\¦ŠÔ[s]
+
+	// PÚ‹ß
+	if (state.status.V + state.status.A * psec < speed) {
+		state.panel[static_cast<size_t>(panelIndex::Pattern)] = false;
+		return 0;
+	}
+	state.panel[static_cast<size_t>(panelIndex::Pattern)] = true;
+
+	// P”ñÚG
+	if (state.status.V < speed) return 0;
+
+	// ATCİ’è
+
+	// ATC—áŠO
+	switch (param) {
+	default:
+	case 0:
+		break;
+	case 1:	// ATCí—p
+		return atsPlugin->getSpec().J;
+		break;
+	case 2:	// ATC”ñí
+		return atsPlugin->getSpec().E;
+		break;
+	}
+
+	// 10km/hˆÈ‰º‚Ì‚Ì—áŠO
+	if (state.status.V < 10) {	// ‚à‚¤‚¿‚å‚Á‚Æl‚¦‚Ä‚à‚¢‚¢‚©‚à
+		ret = atsPlugin->getHandleControl().B;	// ‘O‰ño—Í’l
+		if(state.status.V < 0.25) if (state.status.A == 0) ret = atsPlugin->getSpec().J / 2;
+		else if (state.status.V < 1) if (ret > atsPlugin->getSpec().J / 2) ret = atsPlugin->getSpec().J / 2;
+		else if (state.status.V < 2.5) if (std::rand() % 4) ret--;
+		else if (state.status.V < 5) if (std::rand() % 8) ret--;
+		else if (std::rand() % 16) ret--;
+	}
+
+	// PÚGEP‰‰Z
+	double bl = 5.00 / (atsPlugin->getSpec().E / 2);	// ƒuƒŒ[ƒLŒvZ’l
+	// §ŒÀ+5km/h‚Ì‚É”ñí/2‚Ìo—Í‚É‚È‚é‚æ‚¤‚ÉŠe’i‚ÌP‚ğˆø‚­
+	for (size_t i = 0; i < atsPlugin->getSpec().E; i++) {
+		if (state.status.V - bl * i < speed) {
+			return static_cast<uint16_t>(i);
+		}
+		ret++;
+	}
+
+	return ret;	// g‚í‚È‚¢‚¯‚Ç•ÛŒ¯
+}
+
 R_ATC::R_ATC() {
 }
 
@@ -9,9 +69,23 @@ R_ATC::~R_ATC() {
 
 Hand R_ATC::Elapse(VehicleState state) {
 	Hand ret = Hand();
-	if (atsPlugin->getDoor()) {	// “]“®–h~
-		ret.B = atsPlugin->getSpec().E / 2;
-		ret.P = 0;
+
+	// “]“®–h~
+	if (atsPlugin->getDoor() && state.status.A) {
+		ret.B = atsPlugin->getSpec().E / 2;	//“]“®–h~“®ì
+		state.panel[static_cast<size_t>(panelIndex::Rolling)] = true;	// “]“®–h~“_“”
+	} else if(atsPlugin->getHandleManual().B <= atsPlugin->getSpec().E / 2) {
+		state.panel[static_cast<size_t>(panelIndex::Rolling)] = false;	// “]“®–h~–Å“”
 	}
+
+	ret.B = this->calclateBrake(state, 0/* §ŒÀ‘¬“x */);
+	
 	return ret;
 }
+
+// ƒpƒ^[ƒ“ˆø‚«•û
+// “™‰Á‘¬“x‰^“®‚ğ‚µ‚Än•bŒã‚Éƒpƒ^[ƒ“’ïG: PÚ‹ß
+// §ŒÀ}0: B0
+// §ŒÀ+05: EB / 2
+// §ŒÀ+10: Bmax
+// §ŒÀ+15: EB
--- R-ATC/R_ATC.h
+++ R-ATC/R_ATC.h
@@ -1,11 +1,46 @@
 #pragma once
 class R_ATC {
 private:
+	/* ----- numbers ----- */
+	// R_ATC::limit のインデックス
+	enum struct limitIndex : uint8_t {
+		route,	// 線間最高
+		curve,	// 曲線
+		step2,	// 2段
+		size	// 総数
+	};
+
+	// panelのインデックス
+	enum struct panelIndex : uint8_t {
+		Rolling = 65,	// 転動防止
+		Limit_5 = 66,	// 速度矢印5k
+		Limit_1 = 67,	// 速度矢印1k
+		Pattern = 68,	// P接近
+	};
+
 	/* ----- variants ----- */
+	std::vector<uint16_t> limit;	// 速度制限
+	
+	/* ----- functions ----- */
+	// calclate speed limit
+	// arg : VehicleState state
+	//     vehicle status
+	// return : uint16_t
+	//     output speed limit
+	uint16_t calclateSpeed(VehicleState state);
+
+	// calclate brake notch
+	// arg : VehicleState state
+	//     vehicle status
+	// arg : uint16_t speed
+	//     limit speed
+	// arg : uint8_t param
+	//     ATC parameter
+	// return : uint16_t
+	//     output brake notch
+	uint16_t calclateBrake(VehicleState state, uint16_t speed, uint8_t param = 0);
 
 public:
-	/* ----- functions ----- */
-
 	// constructor
 	R_ATC();
 
